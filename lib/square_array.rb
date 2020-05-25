16b616bd61bc551dd69560b8c5b64029446a58e0
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-25 16:51:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,3 +7,13 @@ while counter < array.length  do
 end 
 new_array 
 end
+
+def square_array(array)
+  new_array = []
+  counter = 0 
+  while counter < array.length do
+    new_array.push(array[counter]**2)
+    counter += 1 
+  end
+  new_array
+end
